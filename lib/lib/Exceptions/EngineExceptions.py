6e4b037238aaa2394b8cfbfa6638e0c6dcb090bd
alevtinamuz <113981269+alevtinamuz@users.noreply.github.com>
2023-06-12 11:35:25 +1000

    Delete lib/lib directory

--- lib/lib/Exceptions/EngineExceptions.py
+++
@@ -1,6 +0,0 @@
-class EngineExceptions(Exception):
-    GET_PROPERTY_ERROR = "Can't get an object property."
-    SET_PROPERTY_ERROR = "Can't set new object property."
-    REMOVE_PROPERTY_ERROR = "Can't remove object property."
-    ENTITY_NOT_EXIST = "Can't find this entity."
-    ENTITY_LIST_ERROR = "Entity list hasn't any entities."
